c150f85098c9d3aadc59583bb48589cae2288565
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-06-15 22:32:35 +0900

    feat: queue

---
+++ queue.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define QUEUE_SIZE 100
+typedef struct{
+	int front;
+	int rear;
+	int array[QUEUE_SIZE];
+} Queue;
+
+Queue init(){
+	Queue queue = {.front=0, .rear=0, .array={0}}; // もし、arrayの型が`int *`でかつinitないのスタック領域へのポインタなら解放されてダングリングポインタとなる
+						      // しかし、今回の場合は、値となるのでダングリングポインタにはならない
+	return queue;
+}
+
+int is_empty(Queue queue){
+	return (queue.rear == queue.front);
+}
+
+int is_full(Queue queue){
+	return (((queue.rear + 1) % QUEUE_SIZE) == queue.front);
+}
+
+Queue enqueue(int value, Queue queue){
+	if(is_full(queue)) return queue;
+	queue.array[queue.rear] = value;
+	queue.rear = (queue.rear + 1) % QUEUE_SIZE;
+	return queue;
+}
+
+int dequeue(Queue *queue){
+	if(is_empty(*queue)) exit(1);
+	int value = queue->array[queue->front];
+	queue->front = (queue->front + 1) % QUEUE_SIZE;
+	return value;
+}
+
+int main(){
+	Queue queue = init();
+	printf("is_empty -> %d\n", is_empty(queue));
+	int values[10] = {-1, 0, 2, 3, 4,
+			  10, 3, -3, 11, 12};
+	int i;
+	for(i=0; i < 5; ++i) queue = enqueue(values[i], queue);
+	for(int j = 0; j < 2; ++j) printf("%d ", dequeue(&queue));
+	printf("is_empty -> %d\n", is_empty(queue));
+	for(;i<10;++i) queue=enqueue(values[i], queue);
+	for(int j = 2; j < 10; ++j) printf("%d ", dequeue(&queue));
+	printf("is_empty -> %d\n", is_empty(queue));
+
+	return 0;
+}
+
