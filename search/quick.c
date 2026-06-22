4887a8779c2a86e8556970469b708fb3a748d198
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-06-22 23:48:58 +0900

    chore!

--- search/quick.c
+++ search/quick.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void quick(int array[], int *l, int *r){
+void quick(int array[], int size){
+	int *l = array, *r = array + size - 1, *pivot = r;
 	if(l >= r) return;
-	int *pivot = r, *il = l, *ir = r;
 	for(;;){
 		for(;*l<*pivot;++l);
-		for(;*pivot<=*r&&il<=r;--r);
+		for(;*pivot<=*r&&array<=r;--r);
 		if(l>=r) break;
 		int t = *l;
 		*l = *r;
@@ -14,14 +14,14 @@ void quick(int array[], int *l, int *r){
 	int t = *pivot;
 	*pivot = *l;
 	*l = t;
-	quick(array,il,l-1);
-	quick(array,l+1,ir);
+	quick(array,l-array);
+	quick(l+1,(array+size-1)-l);
 }
 
 int main(){
 	#define SIZE 10
 	int array[SIZE] = {20,41,19,11,21,9,29,10,93,39};
-	quick(array, array, array + (SIZE-1));
+	quick(array, SIZE);
 	for(int i = 0; i < SIZE; ++i)
 		printf("%d ", array[i]);
 
