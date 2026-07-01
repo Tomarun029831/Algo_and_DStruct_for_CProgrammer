d4599c8bfbd710b0dc574bbf8009b978ed4a1122
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-01 14:43:44 +0900

    feat: brute_force_search

---
+++ string_search/brute_force_search.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+const char *brute_force_search(const char *text, const char *pattern){
+	if(text==NULL || pattern==NULL) return NULL;
+	for(const char *t=text;;++t){
+		if(*t=='\0') return NULL;
+		for(const char *c=t, *p=pattern;;++c,++p)
+			if(*p=='\0') return t;
+			else if(*c!=*p) break;
+	}
+}
+
+int main(){
+	const char *text = "ababbfdsbafkjdsababcfksabdfjseab";
+	const char *pattern = "abc";
+	const char *result = brute_force_search(text, pattern);
+	if(result == NULL) return 0;
+	printf("%d\n", result-text);
+	for(int i = 0; i < 3; ++i)
+		printf("%c", *(result + i));
+
+	return 0;
+}
