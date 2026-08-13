175b96d26d9e1e2fa9e0fb088d8ab244806bbf0e
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-08-13 23:28:07 +0900

    chore:

--- graphic/bresenham_line/bresenham.c
+++ graphic/bresenham_line/bresenham.c
@@ -120,26 +120,20 @@ static void display(const _Bool framebuffer[HEIGHT][WIDTH]){
 	putchar('\n');
 }
 
-static void drawline_bresenham(const Point *const first, const Point *const last, _Bool framebuffer[HEIGHT][WIDTH]) {
-    const int dx = last->x - first->x;
-    const int dy = last->y - first->y;
-    const int steps = (dx > dy) ? dx : dy;
-    const int inc_E  = 2 * dy;
-    const int inc_NE = 2 * (dy - dx);
+static void drawline_bresenham(const Point *const first, const Point *const last, _Bool framebuffer[HEIGHT][WIDTH]){
+	const int dx=last->x-first->x,
+			  dy=last->y-first->y;
+	int D = 2*dy - dx,
+		current_y = first->y;
 
-    int D = 2 * dy - dx;
-    int current_x = first->x;
-    int current_y = first->y;
-
-    for (int t = 0; t <= steps; ++t) {
-        FRAMEBUFFER(current_x, current_y) = 1;
-		if (D < 0) { D += inc_E; }
+	for (int current_x = first->x; current_x<=last->x; ++current_x) {
+		FRAMEBUFFER(current_x, current_y) = 1;
+		if(D<0) D += 2*dy;
 		else {
-            D += inc_NE;
-            ++current_y;
-        }
-        ++current_x;
-    }
+			D += 2*dy-2*dx;
+			current_y += 1;
+		}
+	}
 }
 
 int main(){
