9eafe3e3adbcf34e4ce251dfc1ef3e3a31c9b3e4
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-08-14 13:35:42 +0900

    feat: alois_zingle version is too diffcult for me.

--- graphic/bresenham_line/bresenham_Alois_Zingl.c
+++ graphic/bresenham_line/bresenham_Alois_Zingl.c
@@ -6,8 +6,9 @@
 
 /*
 https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-https://zingl.github.io/Bresenham.pdf
 
+難しすぎる
+https://zingl.github.io/Bresenham.pdf
 
 */
 
@@ -38,6 +39,18 @@ static void display(const _Bool framebuffer[HEIGHT][WIDTH]){
 }
 
 static void drawline_bresenham(const Point *const first, const Point *const last, _Bool framebuffer[HEIGHT][WIDTH]){
+	int x0 = first->x, y0 = first->y, x1 = last->x, y1 = last->y;
+	int dx =  abs(x1-x0), sx = x0 < x1 ? 1 : -1;
+	int dy = -abs(y1-y0), sy = y0 < y1 ? 1 : -1; 
+	int err = dx+dy, e2; /* error value e_xy */
+	
+	for(;;){  /* loop */
+		FRAMEBUFFER(x0, y0) = 1;
+		if (x0==x1 && y0==y1) break;
+		e2 = 2*err;
+		if (e2 >= dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
+		if (e2 <= dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
+	}
 }
 
 int main(){
